132bb8efabf86fae2c2f60f78e4eb9544bb3b784
Anuj9675 <anujupadhyay6265@gmail.com>
2024-08-03 14:14:17 +0530

    changes

--- webcam-app/src/components/webcamCapture.jsx
+++ webcam-app/src/components/webcamCapture.jsx
@@ -202,10 +202,10 @@ const WebcamCapture = () => {
   };
 
   return (
-    <div className="relative w-screen h-screen flex flex-col items-center justify-center">
+    <div className="relative min-w-screen h-screen flex flex-col items-center justify-center overflow-hidden">
       <video
         ref={videoRef}
-        className={`relative h-full w-full object-cover  ${!cameraOn && 'hidden'}`}
+        className={`relative h-full w-full object-cover ${!cameraOn && 'hidden'}`}
       />
       <canvas
         ref={canvasRef}
